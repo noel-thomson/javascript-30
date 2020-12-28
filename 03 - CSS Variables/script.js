5eff5d832f40d71fef81952f4e2d0dcf71533183
Noel Thomson <noel.thomson@gmail.com>
2020-12-27 21:10:57 -0500

    css variables

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,9 +1,16 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleChange() {
-  console.log(this.value);
+  const suffix = this.dataset.suffix || '';
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    `${this.value}${suffix}`
+  );
 }
 
 inputs.forEach((input) => {
   input.addEventListener('change', handleChange);
+});
+inputs.forEach((input) => {
+  input.addEventListener('mousemove', handleChange);
 });
